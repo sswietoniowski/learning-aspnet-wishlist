81732c934e412f79ca5c97b40f0e3786a1b8b902
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-01-03 15:06:59 +0100

    Created home views and home controller

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Index();
+        }
+
+        public IActionResult Error()
+        {
+            return Error();
+        }
+    }
+}
