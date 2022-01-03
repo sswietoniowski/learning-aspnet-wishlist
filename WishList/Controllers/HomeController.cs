530918bca03130da3b4518c4cea725552bd2a008
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-01-03 15:32:27 +0100

    Fixed home controller

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return Index();
+            return View();
         }
 
         public IActionResult Error()
         {
-            return Error();
+            return View();
         }
     }
 }
